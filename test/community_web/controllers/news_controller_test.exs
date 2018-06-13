4ac17caf1f75148a8778d140cbb645a42b0dccf8
Greg <gheidorn@cars.com>
2018-06-12 23:29:07 -0500

    added test for controller

---
+++ test/community_web/controllers/news_controller_test.exs
@@ -0,0 +1,11 @@
+defmodule CommunityWeb.NewsControllerTest do
+
+    use CommunityWeb.ConnCase
+
+    test "GET /", %{conn: conn} do
+        conn = get conn, "/"
+        assert html_response(conn, 200) =~ "from Phoenix!"
+        assert html_response(conn, 200) =~ "bootstrap.min.css"
+    end
+
+end
